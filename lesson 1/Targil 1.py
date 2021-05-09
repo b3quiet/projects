b465cb3839ba438677d0873460fa85dcd86d3225
97254 <ext4zy69@gmail.com>
2021-05-09 12:07:48 +0300

    first one

--- lesson 1/Targil 1.py
+++ lesson 1/Targil 1.py
@@ -1,1 +1,35 @@
 print("Hello world its me")
+echo
+"# projects" >> README.md
+git
+init
+git
+add
+README.md
+git
+commit - m
+"first commit"
+git
+branch - M
+main
+git
+remote
+add
+origin
+https: // github.com / b3quiet / projects.git
+git
+push - u
+origin
+main
+git
+remote
+add
+origin
+https: // github.com / b3quiet / projects.git
+git
+branch - M
+main
+git
+push - u
+origin
+main
